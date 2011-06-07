a0f1e3d4d710669948e0d4e3ebc8ae2a69726df3
Richard Dallaway <richard@dallaway.com>
2011-06-07 14:28:50 +0100

    Upgrade to Lift 2.3

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,7 +3,7 @@ import de.element34.sbteclipsify._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   
-  val liftVersion = "2.2"
+  val liftVersion = "2.3"
 
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
@@ -19,5 +19,15 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
     "org.slf4j" % "slf4j-log4j12" % "1.6.1"
 
   ) ++ super.libraryDependencies
+
+ // To publish to the cloudbees repos:
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "liftmodules repository" at "https://repository-liftmodules.forge.cloudbees.com/release/"
+
+  lazy val repo_user = systemOptional[String]("repo.user", "USERNAME NOT SET")
+  lazy val repo_password = systemOptional[String]("repo.password", "PASSWORD NOT SET")
+
+  // The name and domain format here are not arbitrary:
+  Credentials.add("liftmodules repository", "repository-liftmodules.forge.cloudbees.com", repo_user.value, repo_password.value)
 
 }
